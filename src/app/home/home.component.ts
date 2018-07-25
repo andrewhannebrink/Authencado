1050ad6d2e8e4e826fab384b82e35a2e73ac55b2
Andrew Hannebrink <andrewhannebrink@gmail.com>
2018-07-25 08:09:53 -0700

    settings bonus nav acting totally responsive and links correctly. it also updates when changed pre-screen sizing

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { Router } from '@angular/router';
+import { MatTabChangeEvent } from '@angular/material';
+
 
 @Component({
   selector: 'app-home',
@@ -9,6 +11,7 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
+  public BONUS_NAV_VIEWS = BONUS_NAV_VIEWS;
   public VIEWS = VIEWS;
 
   public currentView: string;
@@ -24,6 +27,24 @@ export class HomeComponent implements OnInit {
     }
   }
 
+  bonusNavSettingsTabChanged(tabChangeEvent: MatTabChangeEvent): void {
+    if (tabChangeEvent.index === 0) {
+      this.changeView(VIEWS.ACCOUNT_DETAILS);
+    } else if (tabChangeEvent.index === 1) {
+      this.changeView(VIEWS.PASSWORD);
+    } else {
+      this.changeView(VIEWS.PAYMENT);
+    }
+  }
+
+  getBonusNavSettingsTabIndexFromCurrentView(currentView: string): number {
+    if (BONUS_NAV_VIEWS.SETTINGS.includes(currentView)) {
+      return BONUS_NAV_VIEWS.SETTINGS.indexOf(currentView);
+    }
+
+    return 0;
+  }
+
   ngOnInit() { }
 
 }
@@ -36,4 +57,16 @@ export const VIEWS = {
   PASSWORD: 'PASSWORD',
   PAYMENT: 'PAYMENT',
   PROFILE: 'PROFILE'
+};
+
+const BONUS_NAV_VIEWS = {
+  SETTINGS: [
+    VIEWS.ACCOUNT_DETAILS,
+    VIEWS.PASSWORD,
+    VIEWS.PAYMENT,
+  ],
+  DASHBOARD: [
+    VIEWS.EXPLORE,
+    VIEWS.PROFILE 
+  ]
 }
