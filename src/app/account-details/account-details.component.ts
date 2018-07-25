5412ae1c824d4501b6281a82587d54874a7c6d86
Andrew Hannebrink <andrewhannebrink@gmail.com>
2018-07-24 19:50:54 -0700

    added account details template and cancel button

--- src/app/account-details/account-details.component.ts
+++ src/app/account-details/account-details.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit, Input} from '@angular/core';
+import { VIEWS } from '../home/home.component';
+
 
 @Component({
   selector: 'app-account-details',
   templateUrl: './account-details.component.html',
-  styleUrls: ['./account-details.component.css', '../app.component.scss']
+  styleUrls: ['./account-details.component.scss', '../app.component.scss']
 })
 export class AccountDetailsComponent implements OnInit {
 
-  @Input() changeView: Function;
+  @Input() changeAuthView: Function;
+
+  public AUTH_VIEWS = VIEWS;
 
   constructor() { }
 
